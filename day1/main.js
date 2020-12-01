fb0da639c3f81bd5929f1b9b77af3c2773f189e9
Szepesi Tibor <sztibi023@gmail.com>
2020-12-01 06:13:27 +0100

    feat: day1

--- day1/main.js
+++ day1/main.js
@@ -1,2 +1,13 @@
-console.log('Hello, World!')
+const fs = require('fs')
 
+const data = fs.readFileSync('data.txt', { encoding: 'utf8' }).split('\n').map(x => Number(x))
+
+for (let i = 0; i < data.length; i++) {
+  for (let j = 0; j < data.length; j++) {
+    for (let k = 0;  k < data.length; k++) {
+      if (data[i] + data[j] + data[k] === 2020) {
+        console.log(data[i] * data[j] * data[k])
+      }
+    }
+  }
+}
