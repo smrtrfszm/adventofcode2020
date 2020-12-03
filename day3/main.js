a075a7e659ef744cc58e04fad9a2dca99235e378
Szepesi Tibor <sztibi023@gmail.com>
2020-12-03 06:23:39 +0100

    feat: day 3 cleanup

--- day3/main.js
+++ day3/main.js
@@ -2,17 +2,12 @@ const fs = require('fs')
 
 const data = fs.readFileSync('data.txt', { encoding: 'utf8' }).split('\n')
 
+const slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
+
 let b = Array(5).fill(0);
-for (let i = 0, a = 0; i < data.length; i++, a++)
-  if (data[i][a % 31] === '#') b[0]++;
-for (let i = 0, a = 0; i < data.length; i++, a += 3)
-  if (data[i][a % 31] === '#') b[1]++;
-for (let i = 0, a = 0; i < data.length; i++, a += 5)
-  if (data[i][a % 31] === '#') b[2]++;
-for (let i = 0, a = 0; i < data.length; i++, a += 7)
-  if (data[i][a % 31] === '#') b[3]++;
-for (let i = 0, a = 0; i < data.length; i+=2, a += 1)
-  if (data[i][a % 31] === '#') b[4]++;
+for (let j = 0; j < slopes.length; j++)
+  for (let i = 0, a = 0; i < data.length; i += slopes[j][1], a += slopes[j][0])
+    if (data[i][a % 31] === '#') b[j]++;
 
 console.log(`1. feladat: ${b[1]}`)
 console.log(`2. feladat: ${b.reduce((acc, a) => acc * a)}`)
