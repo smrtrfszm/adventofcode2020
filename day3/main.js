460a8a3f847cf89d4f4d3af7b030f95d8eaf2121
Szepesi Tibor <sztibi023@gmail.com>
2020-12-03 09:43:42 +0100

    feat: update array construction

--- day3/main.js
+++ day3/main.js
@@ -4,7 +4,7 @@ const data = fs.readFileSync('data.txt', { encoding: 'utf8' }).split('\n')
 
 const slopes = [[1,1],[3,1],[5,1],[7,1],[1,2]]
 
-let b = Array(5).fill(0);
+let b = Array(slopes.length).fill(0);
 for (let j = 0; j < slopes.length; j++)
   for (let i = 0, a = 0; i < data.length; i += slopes[j][1], a += slopes[j][0])
     if (data[i][a % 31] === '#') b[j]++;
