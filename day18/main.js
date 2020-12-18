8046d50ce99fdc4101ef8cefdd9fb1bbc8ab4797
Szepesi Tibor <sztibi023@gmail.com>
2020-12-18 08:11:06 +0100

    feat: day 18 cleanup

--- day18/main.js
+++ day18/main.js
@@ -7,12 +7,12 @@ const data2 = JSON.parse(JSON.stringify(data))
 const solve = (input) => {
   const a = input.match(/[^()]+/g)[0]
   const split = a.split(' ')
-  let b = BigInt(split[0])
+  let b = Number(split[0])
   for (let i = 1; i < split.length; i+=2) {
     if (split[i] === '*') {
-      b *= BigInt(split[i+1])
+      b *= Number(split[i+1])
     } else if (split[i] === '+') {
-      b += BigInt(split[i + 1])
+      b += Number(split[i + 1])
     }
   }
 
@@ -39,8 +39,8 @@ const solve2 = (input) => {
   return a
 }
 
-let p1 = 0n
-let p2 = 0n
+let p1 = 0
+let p2 = 0
 
 for (let i = 0; i < data.length; i++) {
   while (true) {
@@ -48,11 +48,11 @@ for (let i = 0; i < data.length; i++) {
     if (!a) break
     for (const b of a) {
       const solution = solve(b)
-      const find = b.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+      const find = b.replace(/[()*+]/g, '\\$&')
       data[i] = data[i].replace(new RegExp(find, 'g'), solution)
     }
   }
-  p1 += BigInt(solve(data[i]))
+  p1 += Number(solve(data[i]))
 }
 
 for (let i = 0; i < data2.length; i++) {
@@ -61,12 +61,11 @@ for (let i = 0; i < data2.length; i++) {
     if (!a) break
     for (const b of a) {
       const solution = solve2(b)
-      const find = b.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+      const find = b.replace(/[()*+]/g, '\\$&')
       data2[i] = data2[i].replace(new RegExp(find, 'g'), solution)
     }
   }
-  const c = BigInt(solve2(data2[i]))
-  p2 += c
+  p2 += Number(solve2(data2[i]))
 }
 
 console.log(`1. feladat: ${p1}`)
